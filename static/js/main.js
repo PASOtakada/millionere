be9daf25c23530cd79e20756f23c51578eee2e93
takada <masanari.815@gmail.com>
2019-09-03 23:50:31 +0900

    first

---
+++ static/js/main.js
@@ -0,0 +1,38 @@
+
+  document.getElementById('play1').addEventListener('click', function () {
+    var audio = document.getElementById('sound1');
+    audio.play();
+  });
+
+  document.getElementById('play2').addEventListener('click', function () {
+    var audio = document.getElementById('sound2');
+    audio.play();
+  });
+
+  document.getElementById('play3').addEventListener('click', function () {
+    var audio = document.getElementById('sound3');
+    audio.play();
+  });
+
+  document.getElementById('play4').addEventListener('click', function () {
+    var audio = document.getElementById('sound4');
+    audio.play();
+  });
+
+  document.getElementById('play5').addEventListener('click', function () {
+    var audio = document.getElementById('sound5');
+    audio.play();
+  });
+  document.getElementById('play6').addEventListener('click', function () {
+    var audio = document.getElementById('sound6');
+    audio.play();
+  });
+
+  document.getElementById('play7').addEventListener('click', function () {
+    var audio = document.getElementById('sound7');
+    audio.play();
+  });
+  document.getElementById('play8').addEventListener('click', function () {
+    var audio = document.getElementById('sound8');
+    audio.play();
+  });
